958fa82a28b1a000637b7e65479f596d8e5e8e55
Rebecca <rebecca_nhan@rogers.com>
2024-11-28 11:33:56 -0500

    Starting files

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import '../App.css';
+import Header from '../components/Header'
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+        Recipes
+    </div>
+  )
+}
+
+export default Home
