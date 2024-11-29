013520b7f4b82848171a985014e0bc88912012f5
Rebecca <rebecca_nhan@rogers.com>
2024-11-28 23:56:21 -0500

    Added new recipes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import Header from '../components/Header'
 import Featured from '../components/Featured'
+import NewRecipes from '../components/NewRecipes'
 
 function Home() {
   return (
     <div>
         <Header/>
         <Featured/>
+        <hr class="mt-5"></hr>
+        <NewRecipes/>
     </div>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-indigo': '#1e1b4b',
+      },
+    },
   },
   plugins: [],
 }
