c73d72ed0fb25c518cf786058d15af113a64a63e
Rebecca <rebecca_nhan@rogers.com>
2024-12-14 12:22:22 -0500

    Edited cors options

--- backend/server.js
+++ backend/server.js
@@ -15,26 +15,17 @@ const BASE_URL = "/uploads/"
 const app = express();
 
 app.use(express.json());
-app.use(express.static('uploads'));
 app.use(cookieParser());
 
-var whitelist = ['http://localhost:3000', 'https://www.rebbychayrecipes.com', 'http://www.rebbychayrecipes.com', 'https://recipes-frontend-ykxm.onrender.com', 'http://recipes-frontend-ykxm.onrender.com']
-
 var corsOptions = {
-  credentials: true,
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
+  origin: ['http://localhost:3000', 'https://www.rebbychayrecipes.com', 'http://www.rebbychayrecipes.com', 'https://recipes-frontend-ykxm.onrender.com', 'http://recipes-frontend-ykxm.onrender.com']
 }
 
-// app.use(cors())
 app.use(cors(corsOptions));
 
 connectDB();
+
+app.use(express.static('uploads'));
 
 
 // API routers, located in /routes folder
