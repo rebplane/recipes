d5988aa24e18fa436b721f1cc106dcef57a551aa
Rebecca <rebecca_nhan@rogers.com>
2024-12-19 16:54:51 -0500

    Added cute picture at the bottom of the recipe page

--- frontend/src/pages/RecipePage.jsx
+++ frontend/src/pages/RecipePage.jsx
@@ -105,6 +105,55 @@ function RecipePage() {
                             
                             ))}
 
+                            <div className="mt-10">
+                                <div class="max-w-screen-xl w-screen relative flex items-center my-20">
+                                    <div class="mr-5 flex-grow border-t border-3 border-indigo-950"></div> <img src="/images/rebbychay.png" class="h-56" alt="RebbyChayLogo" /> <div class="ml-5 flex-grow border-t border-3 border-indigo-950"></div>
+                                </div>
+                            </div>
+                            {/* <div>
+                                <h1 class="font-bold text-3xl md:mt-0 font-poppins">Reviews (0)</h1>
+
+                                <div class="flex text-orange-700">
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                    </svg>
+                                
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                    </svg>
+                                
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <defs>
+                                        <linearGradient id="grad">
+                                            <stop offset="50%" stop-color="orange"/>
+                                            <stop offset="50%" stop-color="white"/>
+                                        </linearGradient>
+                                    </defs>
+                                    <path fill="url(#grad)" stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                    </svg>
+
+                                </div>
+
+                                My Rating
+                                stars
+                                My Review
+                                submit
+
+                                4.8 out of 5 stars (with visual)
+                                num ratings
+
+                                5 star (bar) (num 5)
+                                4 star (bar) (num 4)
+                                3 star (bar) (num 3)
+                                2 star (bar) (num 2)
+                                1 star (bar) (num 1)
+
+                                linebreak
+
+                                start injecting recipes     sort / filter
+                            </div> */}
+
 
                         </div>
                         
