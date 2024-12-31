1dd227a04f34ea9313d3d5c937abd1fa72adaf07
Rebecca <rebecca_nhan@rogers.com>
2024-12-31 13:23:04 -0500

    Edited about page to look cleaner

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -7,13 +7,13 @@ function About() {
   return (
     <div>
         <Header/>
-            <div className="h-screen flex flex-col mt-20 ml-20 md:mb-0 mb-96">
+            <div className="h-screen flex flex-col mt-20 ml-20 md:mb-0 mb-96 max-md:mx-5 ">
                 <div className="md:flex">
-                        <img src="/images/rebbychay.png" class="md:mt-36 md:h-3/4 h-96 w-96 border border-black" alt="RebbyChayLogo" />
-                    <div className="md:ml-20">
+                        <img src="/images/rebbychay.png" class="md:h-3/4 h-96 w-96 border border-black" alt="RebbyChayLogo" />
+                    <div className="md:ml-20 h-full">
                         <h1 className="text-6xl font-poppins md:mt-0 mt-10">About Us</h1>
                         <p className="text-2xl mt-10 md:w-1/2">Hello! Thanks for visiting our website. We are Rebby and Chay, two software developers in Canada. Rebby grew up in Canada with Vietnamese parents, and Chay grew up in Greece before moving to Canada a few years ago. </p>
-                        <p className="text-2xl mt-10 md:w-1/2">Growing up with both Chinese and Greek backgrounds, we have both eaten a wide variety of food over the years. With one of us being lactose intolerant, and previously needing to be on a FODMAP diet, we are very aware of food intolerances and how they can affect people, and make eating and cooking feel hopeless.</p>
+                        <p className="text-2xl mt-10 md:w-1/2">Growing up with both Chinese and Greek backgrounds, we have both eaten a wide variety of food over the years. With one of us being lactose intolerant, and previously needing to be on a FODMAP + elimination diet, we are very aware of food intolerances and how they can affect people, and make eating and cooking feel hopeless.</p>
                         <p className="text-2xl mt-10 md:w-1/2">Our recipes are those that we have cooked over the years while trying to find foods that both of us can eat while still being delicious. We hope that you enjoy our recipes!</p>
                     </div>
                 </div>
